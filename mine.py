cf2024805922226dbce1edea26ee51ce8778da95
djohns93 <31007895@sfcollege.edu>
2024-09-03 20:40:44 -0400

    mine.py
    added changes

--- mine.py
+++ mine.py
@@ -1,7 +1,12 @@
 # Write a line of code that prints "Hello world!".
-
+print("Hello world!")
 
 def hello_world():
   # Create a variable that has the value of "Hello world!" so that the function will return the appropriate value.
   
   return variable_name
+
+
+variable_name="Hello_world!"
+
+print(hello_world())
